a4d17013959580a208a7eeab068b0af157c3d083
Jose Gregorio Perez Montilla <solintperez@gmail.com>
2023-11-03 14:32:08 -0400

    correciones en datos

--- database/migrations/2023_11_02_153022_create_roles_table.php
+++ database/migrations/2023_11_02_153022_create_roles_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
-            $table->string('rol');
+            $table->string('rol')->unique();
             $table->string('usuario_creacion');
             $table->string('usuario_modificacion');
             // $table->timestamp('fecha_creacion');
